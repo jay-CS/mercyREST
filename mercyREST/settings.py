681b1923eb2a97307ebf75c317665b90ffea7ca4
Jonathan Bazan <jbazan.min21@gmail.com>
2022-04-29 20:38:00 -0700

    Update settings.py

--- mercyREST/settings.py
+++ mercyREST/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-oi5r%q)8kv=f526m&yw$(0j0%9@^k9!grqn&4*6)&_(cr^fuv6
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(int(os.environ.get('DEBUG',1)))
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
 'ec2-34-229-157-65.compute-1.amazonaws.com']
 
