6feac565390a71910e88903576259b382d55aab0
Jonathan Bazan <jbazan.min21@gmail.com>
2022-04-29 20:31:36 -0700

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -26,8 +26,8 @@ $PROJECT_BASE_PATH/env/bin/pip3 install uwsgi==2.0.18
 
 # Run migrations and collectstatic
 cd $PROJECT_BASE_PATH
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+$PROJECT_BASE_PATH/env/bin/python3 manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python3 manage.py collectstatic --noinput
 
 # Configure supervisor
 cp $PROJECT_BASE_PATH/deploy/supervisor_mercyApp.conf /etc/supervisor/conf.d/mercyApp.conf
